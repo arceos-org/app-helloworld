5a32a69980ccfda26bb58d90f1bf461ed588e7ce
chyyuu <yuchen@tsinghua.edu.cn>
2026-02-06 21:42:54 +0800

    init version: cargo clone arceos-helloworld

--- scripts/get_deps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-AX_ROOT=.arceos
-
-test ! -d "$AX_ROOT" && echo "Cloning repositories ..." || true
-test ! -d "$AX_ROOT" && git clone https://github.com/arceos-org/arceos $AX_ROOT || true
